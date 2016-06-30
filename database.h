341492b88b46b60a9b1c5c435de2c54bc0f9ddd1
lacavazan <lucascpds@gmail.com>
2016-06-30 17:22:45 -0300

    Nois brodi

--- database.h
+++ database.h
@@ -10,6 +10,7 @@
 #define MAX_MESSAGE_QUEUE 20
 #define MAX_PERSON_PER_GROUP 10
 #define MAX_COUNTER 50
+#define MAX_GAMES 10
 
 struct message;
 struct Group;
@@ -23,9 +24,9 @@ typedef struct {
 } person;
 
 typedef enum { new_msg = 0, sent_msg, recv_msg,
-			   newgrp_msg, create_group, join_group, recvgrp_msg,
-			   error_msg, login_msg, exit_msg, who_msg,
-			   game_msg, play_msg, acptg_msg, rfsg_msg, sentg_msg, recvp_msg,
+			   newgrp_msg, create_group, join_group, recvgrp_msg, //6
+			   error_msg, login_msg, exit_msg, who_msg, // 10
+			   invite_player, game_move, accept_invite, refuse_invite, invite_sent, invite_received, move_sent, move_received, won_game, lost_game, draw_game,
 			   file_msg, cont_msg, acptf_msg, rfsf_msg, recvg_msg, sentp_msg 
 } messageType;
 
@@ -44,6 +45,13 @@ typedef struct {
     person *members[MAX_PERSON_PER_GROUP];
 } Group;
 
+typedef enum {B = 0, O, X, D} boardState;
+typedef struct {
+    boardState board[3][3];
+    boardState turn;
+    person *playerA, *playerB;
+} game;
+
 int sendMessageToGroup (message *msg);
 int sendMessageToPerson (message *msg);
 person *getPersonByName (char *name);
@@ -56,3 +64,11 @@ void setupDatabase ();
 Group *getGroupByName (char *name);
 int addUserToGroup (Group *g, person *p);
 void decreaseCounter(message *m);
+
+/********************************* Game *******************************/
+
+boardState getWinner (game *g);
+int makeMove (game *g, int mv[2], person *player);
+game *getGame (person *A, person *B);
+void removeGame (person *playerA, person *playerB);
+int createGame (person *A, person *B);
